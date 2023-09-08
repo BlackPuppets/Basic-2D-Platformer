3305171124f60ce9d183441f0381028fb8c10404
João Vaccari <joaopedrov_vaccari@hotmail.com>
2023-09-08 12:09:31 -0300

    Added Animations controlled via script

---
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private const string IDLE = "a_Idle";
+    private const string RUNNING = "a_Running";
+    private const string ONAIR = "a_OnAir";
+    private const string JUMPING = "a_Jumping";
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+        playerMovement = GetComponent<PlayerMovement>();
+        playerMovement.OnIdleEvent += OnIdleAnimation;
+        playerMovement.OnRunningRightEvent += OnRunningRightAnimation;
+        playerMovement.OnRunningLeftEvent += OnRunningLeftAnimation;
+        playerMovement.OnAirEvent += OnAirAnimation;
+        playerMovement.OnJumpingEvent += OnJumpingAnimation;
+    }
+
+    private void OnIdleAnimation()
+    {
+        animator.Play(IDLE);
+    }
+
+    private void OnRunningRightAnimation()
+    {
+        animator.Play(RUNNING);
+        spriteRenderer.flipX = false;
+    }
+
+    private void OnRunningLeftAnimation()
+    {
+        animator.Play(RUNNING);
+        spriteRenderer.flipX = true;
+    }
+
+    private void OnAirAnimation()
+    {
+        animator.Play(ONAIR);
+    }
+
+    private void OnJumpingAnimation()
+    {
+        animator.Play(JUMPING);
+    }
+
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static PlayerMovement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -17,6 +18,30 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D playerRigidbody2D;
 
+    [Header("GroundedControl")]
+    [SerializeField] public Transform top_left;
+    [SerializeField] public Transform bottom_right;
+    [SerializeField] public LayerMask ground_layers;
+    private bool isGrounded = true;
+
+    #region animationEvents
+    public delegate void OnIdle();
+    public event OnIdle OnIdleEvent;
+
+    public delegate void OnRunningRight();
+    public event OnRunningRight OnRunningRightEvent;
+
+    public delegate void OnRunningLeft();
+    public event OnRunningLeft OnRunningLeftEvent;
+
+    public delegate void OnAir();
+    public event OnAir OnAirEvent;
+
+    public delegate void OnJumping();
+    public event OnJumping OnJumpingEvent;
+
+    #endregion
+
     void Awake()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
@@ -31,6 +56,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         ControlMaxSpeed();
+        ControlIsGrounded();
     }
 
     private void Movement()
@@ -38,25 +64,29 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = false;
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
+            horizontalInput = true;
+            if (isGrounded)
+                OnRunningRightEvent();
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 playerRigidbody2D.AddForce(new Vector2(runningSpeedHorizontal, 0));
                 return;
             }
             playerRigidbody2D.AddForce(new Vector2(speedHorizontal, 0));
-            horizontalInput = true;
         }
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
+            horizontalInput = true;
+            if (isGrounded)
+                OnRunningLeftEvent();
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 playerRigidbody2D.AddForce(new Vector2(-runningSpeedHorizontal, 0));
                 return;
             }
             playerRigidbody2D.AddForce(new Vector2(-speedHorizontal, 0));
-            horizontalInput = true;
         }
-        if (!horizontalInput){
+        else if (!horizontalInput){
             StopHorizontalMovement();
             return;
         }
@@ -70,14 +100,36 @@ public class PlayerMovement : MonoBehaviour
     private void StopHorizontalMovement()
     {
         playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x * 0.8f, playerRigidbody2D.velocity.y);
+        if (isGrounded)
+        {
+            OnIdleEvent();
+        }
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             playerRigidbody2D.AddForce(new Vector2(0, speedJump));
+            OnJumpingEvent();
         }
+    }
+
+    private void ControlIsGrounded()
+    {
+
+        isGrounded = Physics2D.OverlapArea(top_left.position, bottom_right.position, ground_layers);
+
+        if (!isGrounded)
+        {
+            OnAirEvent();
+        }
+
+        if (isGrounded)
+        {
+            playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, 0);
+        }
+
     }
 
 }
